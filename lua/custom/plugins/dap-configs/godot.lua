e75f061f037950110fed7925008adde8b5c65a8f
Ben Clegg <ben@clegg.tech>
2024-01-23 18:13:05 +0000

    godot dap configured

---
+++ lua/custom/plugins/dap-configs/godot.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+function M.setup()
+  dap = require("dap")
+  dap.adapters.godot = {
+    type = "server",
+    host = "127.0.0.1",
+    port = 6006,
+  }
+
+  dap.configurations.gdscript = {
+    {
+      type = "godot",
+      request = "launch",
+      name = "Launch scene",
+      project = "${workspaceFolder}",
+      launch_scene = true,
+    }
+  }
+end
+
+return M
